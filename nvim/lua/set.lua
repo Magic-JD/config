587f73ff467a0bc7e288cd741a108b5a8d783645
MagicJoe <mag1ctr1cks@hotmail.com>
2024-08-17 18:06:49 +0700

    Using unnamed plus

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -26,6 +26,8 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
+vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
+
 vim.opt.updatetime = 50
 vim.opt.showmode = false
 vim.opt.hlsearch = true
