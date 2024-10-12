a3cd2aa3f1e2b95a0bf57b2cff8a0ecddeeb3f5b
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2024-10-13 01:02:20 +0700

    Adding to the install file

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 test -d ~/.linuxbrew
 eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-brew install bat lazygit starship atac zsh-autosuggestions zsh-syntax-highlighting atuin yazi btop fzf zoxide fd tmux ripgrep stow eza git-delta miller;
+brew install bat lazygit starship atac zsh-autosuggestions zsh-syntax-highlighting atuin yazi btop fzf zoxide fd tmux ripgrep stow eza git-delta miller zsh-vi-mode jq zsh;
 
 mkdir -p "$(bat --config-dir)/themes" && wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme && bat cache --build;
 
