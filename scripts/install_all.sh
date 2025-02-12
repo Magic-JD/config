1ff9541b272a64a81ff52fb0f247ec4fe1feed48
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2025-02-12 12:09:39 +0800

    Adding posting to the install files and config

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -6,7 +6,9 @@ eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-brew install bat lazygit starship atac zsh-autosuggestions zsh-syntax-highlighting atuin yazi btop fzf zoxide fd tmux ripgrep stow eza git-delta miller zsh-vi-mode jq zsh;
+brew install bat lazygit starship atac zsh-autosuggestions zsh-syntax-highlighting atuin yazi btop fzf zoxide fd tmux ripgrep stow eza git-delta miller zsh-vi-mode jq zsh uv;
+
+uv tool install --python 3.12 posting;
 
 mkdir -p "$(bat --config-dir)/themes" && wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme && bat cache --build;
 
